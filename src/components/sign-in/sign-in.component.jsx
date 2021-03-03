5fdc50e00620c2c83b2f2ad8417c630075c9dded
futuregit <gmail.com>
2021-03-03 00:36:09 -0500

    Added backend signin provider

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.util';
+
 import './sign-in.styles.scss';
 
 class SignIn extends Component {
@@ -49,6 +51,9 @@ class SignIn extends Component {
                         required />
 
                     <CustomButton type="submit">Sign in</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} >
+                        {''}
+                     </CustomButton>
                 </form>
             </div>
         )
