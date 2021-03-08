1a21cfa382717327b079d5771b831a3c1d17a434
futuregit <tomcis102@hotmail.com>
2021-03-08 05:03:08 -0500

    Set up authentication with Firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,10 +12,39 @@ const config = {
     measurementId: process.env.REACT_APP_MEASUREMENTID
   };
 
+ 
+
   firebase.initializeApp(config);
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+    const snapShot = await userRef.get();
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    return userRef;
+  };
+
   export const auth = firebase.auth();
-  export const firestone = firebase.firestore();
+  export const firestore = firebase.firestore();
+
+ 
 
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({ prompt: 'select_account' });
