a6a1f9dbfeab1d8467be47fa9c8ed7928426c63b
futuregit <gmail.com>
2021-03-17 08:13:23 -0400

    Added type=button to custom button to prevent error

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,6 +4,7 @@ import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
     <button 
+        type="button"
         className={`${inverted ? 'inverted': ''} ${isGoogleSignIn ? 'google-sign-in': ''} custom-button` }
         {...otherProps }
     >
