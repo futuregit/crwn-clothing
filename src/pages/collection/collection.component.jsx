1313e94483dcce0dd212dea7e6a75973484e34cb
futuregit <tomcis102@hotmail.com>
2021-04-07 21:36:31 -0400

    Updated App js and sign in to use hook

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,9 +6,12 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import { selectCollection, selectCollections } from '../../redux/shop/shop.selectors';
 
+import { firestore } from '../../firebase/firebase.utils';
+
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
+
     const { title, items } = collection;
     return(
     <div className="collection-page">
